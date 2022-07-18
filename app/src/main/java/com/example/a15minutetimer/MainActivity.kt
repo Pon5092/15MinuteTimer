a16019a9721069bcbd157785c18a775ce488c8b7
Pon5092 <tecliz740@gmail.com>
2022-07-18 16:44:36 +0900

    add: ボタンを押せるように

--- app/src/main/java/com/example/a15minutetimer/MainActivity.kt
+++ app/src/main/java/com/example/a15minutetimer/MainActivity.kt
@@ -11,8 +11,30 @@ class MainActivity : AppCompatActivity() {
         //画面を動的に
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var nowTime = "88:88"
+        //残り時間表示を動的になっているか，確認用（88:88）でＯＫ
+        binding.timer.text = nowTime
 
-        //残り時間表示を動的にする
-        binding.timer.text = "88:88"
+        //ボタン押されたときの動作
+        binding.startStop.setOnClickListener {
+            nowTime = "start/stop"
+            binding.timer.text = nowTime
+        }
+        binding.plusOneMin.setOnClickListener {
+            nowTime = "plusOne"
+            binding.timer.text = nowTime
+        }
+        binding.plusFiveMin.setOnClickListener {
+            nowTime = "plusFive"
+            binding.timer.text = nowTime
+        }
+        binding.reset.setOnClickListener {
+            nowTime = "reset"
+            binding.timer.text = nowTime
+        }
+        binding.nextLap.setOnClickListener {
+            nowTime = "nextLap"
+            binding.timer.text = nowTime
+        }
     }
 }
