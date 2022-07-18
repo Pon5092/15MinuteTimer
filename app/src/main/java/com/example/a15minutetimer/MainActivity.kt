9f9e6d90086aafae7ced594a713ef65c18f6660e
Pon5092 <tecliz740@gmail.com>
2022-07-18 15:22:25 +0900

    modify:動的に文字を変更できるように

--- app/src/main/java/com/example/a15minutetimer/MainActivity.kt
+++ app/src/main/java/com/example/a15minutetimer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a15minutetimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a15minutetimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //画面を動的に
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //残り時間表示を動的にする
+        binding.timer.text = "88:88"
     }
 }
