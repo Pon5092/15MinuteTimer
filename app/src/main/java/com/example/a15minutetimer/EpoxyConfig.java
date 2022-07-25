3eb357c751b10354ff93c9a0c8c4d8a4feb08c1f
Pon5092 <tecliz740@gmail.com>
2022-07-25 23:23:03 +0900

    add:Epoxy

---
+++ app/src/main/java/com/example/a15minutetimer/EpoxyConfig.java
@@ -0,0 +1,7 @@
+package com.example.a15minutetimer;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "model")
+interface EpoxyConfig{
+}
